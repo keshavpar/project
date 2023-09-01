9fbe02e5d9797f9f4c52de6b1ec3e2c406031a97
Shubham Goswami <shubham24976@gmail.com>
2023-09-01 12:49:43 +0530

    Removed Protect Middleware

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,16 +8,16 @@ const router = express.Router();
 // Routes
 
 // Fetch all the guests of the user
-router.get('/guests/:userId', authController.protect, userController.guestList);
+router.get('/guests/:userId', userController.guestList);/* authController.protect, */
 
 // update-guests/23fhksfelk
-router.patch('/update-guests/:userId', authController.protect, userController.addGuests);
+router.patch('/update-guests/:userId', userController.addGuests); /* authController.protect */
 
 // update-guests/user/23fhksfelk/guest/98scjbkak
 router.
     route('/update-guests/user/:userId/guest/:guestId')
-    .delete(authController.protect, userController.deleteGuest)
-    .patch(authController.protect, userController.updateGuest);
+    .delete(userController.deleteGuest) /* authController.protect */
+    .patch(userController.updateGuest); /* authController.protect */
 
 
 module.exports = router;
